c3d3e00c0e15960e3a59037b00344ec1fe334ac5
Bhavya Srivastava <111657208+BhavyaSrvstv@users.noreply.github.com>
2026-06-06 15:10:54 +0530

    Add files via upload

---
+++ Assignments/Assignment_1/BhavyaSrivastava_01104092025/5_Sqaure_and_cube.py
@@ -0,0 +1,7 @@
+# Square and Cube of a Number
+
+number = float(input("Enter a number: "))
+square = number ** 2
+cube = number ** 3
+print("Square =", square)
+print("Cube =", cube)
---
+++ Assignments/Assignment_1/BhavyaSrivastava_01104092025/6_Swap.py
@@ -0,0 +1,13 @@
+# Swap Two Numbers Without Third Variable
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before Swapping:")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
